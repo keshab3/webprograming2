ac0428312c6795f632f0874b56cccafbe68c0501
kesha <keshabamgain3@gmail.com>
2026-04-21 11:49:08 +0900

    Verified Version

---
+++ src/Main.kt
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Keshab"
+
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
+
+    fun printSum(a: Int, b: Int) {
+        println("sum of $a and $b is ${a + b}")
+    }
+
+    fun printAge(age: Int) {
+        println("age is $age");
+    }
+
+    // Prints a message to request input
+    println("Enter any word: ")
+
+// Reads and stores the user input. For example: Happiness
+    val yourWord = readln()
+
+// Prints a message with the input
+    print("You entered the word: ")
+    print(yourWord)
+// You entered the word: Happiness
+
+
+
+
+printSum(10, 20);
+    printAge(27);
+
+}
